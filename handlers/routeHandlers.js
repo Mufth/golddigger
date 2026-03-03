7511fc23d2f91e675f68484cee7585d8801700f6
Mufth <jeppepedas@hotmail.com>
2026-03-03 19:39:48 +0100

    liveprice added

---
+++ handlers/routeHandlers.js
@@ -0,0 +1,21 @@
+
+
+export async function handleLive(req, res) {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/event-stream')
+  res.setHeader('Cache-Control', 'no-cache')
+  res.setHeader('Connection', 'keep-alive')
+
+    const sendPrice = () => {
+        const price = (Math.random() * 1000 + 1000).toFixed(2);
+        res.write(`data: ${JSON.stringify({ price })}\n\n`);
+    }
+
+    sendPrice();
+    const interval = setInterval(sendPrice, 5000);
+
+    req.on('close', () => {
+        clearInterval(interval);
+    });
+
+}
